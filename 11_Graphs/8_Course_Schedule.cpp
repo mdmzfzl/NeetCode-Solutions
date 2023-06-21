1d354bed723cb22ae90df11bb3b594017de74eb0
Mohammed Muzakkir <mmuzakkir244@gmail.com>
2023-06-21 19:23:03 +0530

    Added #7, #8 and #9 in Graphs

--- 11_Graphs/8_Course_Schedule.cpp
+++ 11_Graphs/8_Course_Schedule.cpp
@@ -1,21 +1,117 @@
+/**
+ * ? Approach - Using Topological sort
+ * * 1. First we build an adjacency matrix for the given graph.
+ * * 2. We create an array indegree and indegree[i] is the number of edges approaching a node.
+ * * 3. If no node has indegree 0, then a cycle exists and we can definitely not finish all the courses.
+ * * 4. If there's a node with indegree 0, we start with the node, we move to it's child nodes and keep on decrementing their indegrees.
+ * * 5. If in the end, we're able to reach all nodes, that means we can finish all the courses, so we return true.
+ * Time Complexity: O(V + E)
+ * Space Complexity: O(V)
+*/
+
 class Solution {
-public:
-    bool isPalindrome(string s) {
-        int left = 0 , right = s.size() - 1;
-        
-        while(left < right) {
-            // isalnum -> Is alphabet or number
-            if(!isalnum(s[left]))
-                left++;
-            else if(!isalnum(s[right]))
-                right--;
-            else if(tolower(s[left]) != tolower(s[right])) 
-                return false;  
-            else {
-                left++;
-                right--;  
+public: 
+    bool canFinish(int n, vector<vector<int>> &p) {
+        if (p.size() == 0) 
+            return true; //all courses independent
+
+        vector<int> indegree(n, 0); //counts the number of edges approaching a particular node
+        vector<vector<int>> graph(n); //adjacency matrix of the given graph
+    
+        for (auto & x: p) {
+            graph[x[1]].push_back(x[0]);
+            indegree[x[0]]++;
+        }
+
+        queue < int > q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) q.push(i), indegree[i]--;
+        }
+        if (q.size() == 0) return false; //cycle exists if there is no node with indegree 0
+
+        int count = 0; //No. of nodes you're able to reach till the end
+        while (!q.empty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int curr = q.front();
+                q.pop();
+                count++;
+                for (auto child: graph[curr]) {
+                    indegree[child]--; //decrement the indegree of the nodes until you've reached all nodes
+                    if (indegree[child] == 0) q.push(child);
+                }
             }
         }
-        return true;
+        if (count == n) 
+            return true;
+        return false;
     }
 };
+
+/*
+class Solution {
+public:
+  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> adjacency(numCourses);
+    vector<bool> visited(numCourses);
+
+    for (vector<int>& p : prerequisites) {
+      if (p[0] == p[1]) return false;
+      adjacency[p[0]].push_back(p[1]);
+    }
+
+    for (int course = 0; course < numCourses; course++) {
+      if (!dfs(course, adjacency, visited)) return false;
+    }
+
+    return true;
+  }
+
+  bool dfs(int course, vector<vector<int>>& adjacency, vector<bool>& visited) {
+    if (visited[course]) return false;
+
+    visited[course] = true;
+
+    vector<int>& prerequisites = adjacency[course];
+
+    for (int i = prerequisites.size() - 1; i > -1; i--) {
+      if (!dfs(prerequisites[i], adjacency, visited)) return false;
+      prerequisites.pop_back();
+    }
+
+    visited[course] = false;
+
+    return true;
+  }
+};
+*/
+
+/*
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> adj[n],ino(n,0);
+        for(auto p : pre) {
+            adj[p[0]].push_back(p[1]);
+            ino[p[1]]++;
+        }
+        vector<int> topo;
+        queue<int> q;
+        for(int i=0;i<n;i++) {
+            if(ino[i]==0) {
+                q.push(i);
+            } 
+        }
+        while(!q.empty()) {
+            int f = q.front();q.pop();
+            topo.push_back(f);
+            for(auto k : adj[f]) {
+                ino[k]--;
+                if(ino[k]==0) q.push(k);
+            }
+        }
+        return topo.size()==n;
+    }
+};
+*/
--- 11_Graphs/9_Course_Schedule_II.cpp
+++ 11_Graphs/9_Course_Schedule_II.cpp
@@ -1,21 +1,99 @@
+/*
+    All courses can be completed if there's no cycle, check for cycles
+
+    Time: O(V + E)
+    Space: O(V + E)
+*/
+
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int left = 0 , right = s.size() - 1;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> m;
+        // build adjacency list of prereqs
+        for (int i = 0; i < prerequisites.size(); i++) {
+            m[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        unordered_set<int> visit;
+        unordered_set<int> cycle;
         
-        while(left < right) {
-            // isalnum -> Is alphabet or number
-            if(!isalnum(s[left]))
-                left++;
-            else if(!isalnum(s[right]))
-                right--;
-            else if(tolower(s[left]) != tolower(s[right])) 
-                return false;  
-            else {
-                left++;
-                right--;  
+        vector<int> result;
+        for (int course = 0; course < numCourses; course++) {
+            if (!dfs(course, m, visit, cycle, result)) {
+                return {};
             }
         }
+        return result;
+    }
+private:
+    // a course has 3 possible states:
+    // visited -> course added to result
+    // visiting -> course not added to result, but added to cycle
+    // unvisited -> course not added to result or cycle
+    bool dfs(int course, unordered_map<int, vector<int>>& m, unordered_set<int>& visit,
+        unordered_set<int>& cycle, vector<int>& result) {
+        
+        if (cycle.find(course) != cycle.end()) {
+            return false;
+        }
+        if (visit.find(course) != visit.end()) {
+            return true;
+        }
+        cycle.insert(course);
+        for (int i = 0; i < m[course].size(); i++) {
+            int nextCourse = m[course][i];
+            if (!dfs(nextCourse, m, visit, cycle, result)) {
+                return false;
+            }
+        }
+        cycle.erase(course);
+        visit.insert(course);
+        result.push_back(course);
         return true;
     }
 };
+
+/**
+ * ? Approach - Topological sort using DFS
+ * * 1. Initialize a 2D vector graph and result & inDegree vector, to help us in keeping track of whether a node is visited or not.
+ * * 2. Start DFS from a course which has 0 prerequisites i.e the inDegree is 0 and mark it as visited.
+ * * 3. After completion of a course, add the current course to our result. Since we added a course we will reduce the indegree for all the courses whose prerequisites was the current course.
+ * * 4. If the indegree bof a course becomes 0, add them into DFS and start DFS call from that course.
+ * * 5. Repeat the steps by calling dfs again for each course with indegree 0.
+ * * 6. In the end, return result if it contains all numCourses i.e Check size of result. If size == numCourses, return result, else return []
+*/
+/*
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        //Initializing a 2D graph, result and inDegree array
+        vector<vector<int>> graph(numCourses);
+        vector<int> result, inDegree(numCourses);
+        
+        // inDegree
+        for(auto& prereq : prerequisites)
+            graph[prereq[1]].push_back(prereq[0]),
+            inDegree[prereq[0]]++;
+        
+        // DFS call
+        function<void(int)> dfs = [&](int current) {
+            // Adding the current course into the result 
+            result.push_back(current);            
+            // Marking the current course that we added as visited
+            inDegree[current] = -1;                     
+            
+            // If any next course has a indegree of 0 i.e. if it has no prerequisites requirement than make dfs call for that course
+            for(auto nextCourse : graph[current])          
+                if(--inDegree[nextCourse] == 0)     
+                    dfs(nextCourse);                
+        };
+        
+        // If the indegree becomes 0 of a course then again add them into DFS and start DFS call from that course .
+        for(int i = 0; i < numCourses; i++)
+            if(inDegree[i] == 0) dfs(i);                   
+        
+        //In the end return `result` if it contains all `numCourses` else return  []
+        if(size(result) == numCourses) return result;
+        return {};
+    }
+};
+*/
