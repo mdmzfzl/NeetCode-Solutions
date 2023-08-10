39a5a9d7db8cc82e1ef6bc2349b8ab7fd6f4a8e9
Mohammed Muzakkir <mmuzakkir244@gmail.com>
2023-08-10 14:45:01 +0530

    Added scripts to rename files

---
+++ rename_cpp_files.py
@@ -0,0 +1,32 @@
+import os
+import re
+
+# Define the source directory containing subdirectories with C++ files
+source_directory = '/home/larcade/Downloads/NeetCodeee'
+
+# Define a regular expression pattern to extract the problem number and name
+pattern = r'Problem: LeetCode (\d+) - (.+)\n'
+
+# Function to rename C++ files based on problem number and name
+def rename_cpp_files(root, subdirs):
+    for subdir in subdirs:
+        sub_path = os.path.join(root, subdir)
+        if os.path.isdir(sub_path):
+            for filename in os.listdir(sub_path):
+                file_path = os.path.join(sub_path, filename)
+                if filename.endswith('.cpp'):
+                    with open(file_path, 'r') as file:
+                        content = file.read()
+                        match = re.search(pattern, content)
+                        if match:
+                            problem_number = match.group(1).zfill(4)
+                            problem_name = match.group(2).strip().replace(' ', '-').lower()
+                            new_filename = f"{problem_number}-{problem_name}.cpp"
+                            new_path = os.path.join(sub_path, new_filename)
+                            os.rename(file_path, new_path)
+                            print(f"Renamed {filename} to {new_filename}")
+
+# Recursively walk through the source directory and its subdirectories
+for root, dirs, _ in os.walk(source_directory):
+    rename_cpp_files(root, dirs)
+
