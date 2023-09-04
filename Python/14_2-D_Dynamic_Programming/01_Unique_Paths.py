a367bffbb9de2b9c592d0ff0bb22be67c492a69e
Mohammed Muzakkir <mmuzakkir244@gmail.com>
2023-09-04 12:26:49 +0530

    Added explanation and solution in 14_2-D_Dynamic_Programming

---
+++ Python/14_2-D_Dynamic_Programming/01_Unique_Paths.py
@@ -0,0 +1,25 @@
+'''
+Problem: LeetCode 62 - Unique Paths
+
+Key Idea:
+The key idea is to use dynamic programming to count the number of unique paths from the top-left corner to the bottom-right corner of a grid. We create a dynamic programming grid 'dp', where dp[i][j] represents the number of unique paths to reach cell (i, j) from the top-left corner.
+
+Time Complexity:
+The time complexity is O(m*n), where 'm' is the number of rows and 'n' is the number of columns in the grid. We iterate through each cell once to fill in the dp array.
+
+Space Complexity:
+The space complexity is O(m*n) because we use an additional dp grid of the same size as the input grid to store the results.
+'''
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # Initialize a 2D dp grid of size m x n with all values set to 1.
+        dp = [[1] * n for _ in range(m)]
+
+        # Fill in the dp grid using dynamic programming.
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        # The value in dp[m-1][n-1] represents the number of unique paths.
+        return dp[m - 1][n - 1]
---
+++ Python/14_2-D_Dynamic_Programming/02_Longest_Common_Subsequence.py
@@ -0,0 +1,30 @@
+'''
+Problem: LeetCode 1143 - Longest Common Subsequence
+
+Key Idea:
+The key idea is to use dynamic programming to find the length of the longest common subsequence (LCS) between two strings. We create a 2D dp array where dp[i][j] represents the length of the LCS between the first i characters of text1 and the first j characters of text2.
+
+Time Complexity:
+The time complexity is O(m*n), where 'm' is the length of text1 and 'n' is the length of text2. We iterate through both strings once to fill in the dp array.
+
+Space Complexity:
+The space complexity is O(m*n) because we use an additional dp array of the same size as the input strings.
+'''
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        
+        # Create a 2D dp array of size (m+1) x (n+1) and initialize it with zeros.
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        # Fill in the dp array using dynamic programming.
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        # The value in dp[m][n] represents the length of the LCS.
+        return dp[m][n]
