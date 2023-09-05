8fe1a1cff85e3d252414a4b297480db4d23ca0e8
Mohammed Muzakkir <mmuzakkir244@gmail.com>
2023-09-05 14:06:26 +0530

    Added explanation and solution in 14_2-D_Dynamic_Programming

---
+++ Python/14_2-D_Dynamic_Programming/04_Coin_Change_II.py
@@ -0,0 +1,29 @@
+'''
+Problem: LeetCode 518 - Coin Change II
+
+Key Idea:
+The key idea is to use dynamic programming to count the number of combinations that make up each amount from 0 to 'amount'.
+
+Time Complexity:
+The time complexity is O(amount * n), where 'amount' is the target amount and 'n' is the number of coin denominations. We iterate through 'amount' values and for each amount, we iterate through 'n' coin denominations.
+
+Space Complexity:
+The space complexity is O(amount) because we use a 1D array 'dp' of size 'amount + 1' to store the number of combinations for each amount.
+'''
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # Initialize a 1D array dp to store the number of combinations for each amount from 0 to amount.
+        dp = [0] * (amount + 1)
+        
+        # There is one way to make amount 0 (by not using any coins).
+        dp[0] = 1
+        
+        # Iterate through each coin denomination.
+        for coin in coins:
+            # Update the dp array for each amount from coin to amount.
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+        
+        # The dp[amount] contains the number of combinations to make the target amount.
+        return dp[amount]
---
+++ Python/14_2-D_Dynamic_Programming/05_Target_Sum.py
@@ -0,0 +1,39 @@
+'''
+Problem: LeetCode 494 - Target Sum
+
+Key Idea:
+The key idea is to use dynamic programming to count the number of ways to reach a certain sum by assigning either a positive or negative sign to each element in the input array.
+
+Time Complexity:
+The time complexity is O(n * sum), where 'n' is the number of elements in the input array 'nums' and 'sum' is the sum of all elements in 'nums'. We iterate through 'n' elements and, for each element, iterate through 'sum' values.
+
+Space Complexity:
+The space complexity is O(sum) because we use a 1D array 'dp' of size 'sum + 1' to store the number of ways to reach each sum from 0 to 'sum'.
+'''
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        # Calculate the sum of all elements in the input array 'nums'.
+        total_sum = sum(nums)
+        
+        # If the total sum is less than the target sum 'S', it's not possible to reach 'S'.
+        if (total_sum + S) % 2 != 0 or total_sum < abs(S):
+            return 0
+        
+        # Calculate the target sum for positive signs. (total_sum + S) / 2
+        target = (total_sum + S) // 2
+        
+        # Initialize a 1D array 'dp' to store the number of ways to reach each sum from 0 to 'target'.
+        dp = [0] * (target + 1)
+        
+        # There is one way to reach a sum of 0 (by not selecting any element).
+        dp[0] = 1
+        
+        # Iterate through each element in the input array 'nums'.
+        for num in nums:
+            # Update the 'dp' array for each sum from 'target' to 'num'.
+            for i in range(target, num - 1, -1):
+                dp[i] += dp[i - num]
+        
+        # The 'dp[target]' contains the number of ways to reach the target sum 'S'.
+        return dp[target]
