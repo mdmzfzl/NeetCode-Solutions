30692af02b32ef22df4de700657914506f945afc
mdmzfzl <mmuzakkir244@gmail.com>
2023-10-12 11:47:35 +0300

    Added Rust solutions in 12_Advanced_Graphs

---
+++ 12_Advanced_Graphs/04_Swim_in_Rising_Water/0778-swim-in-rising-water.rs
@@ -0,0 +1,70 @@
+/*
+Problem: LeetCode 778 - Swim in Rising Water
+
+Key Idea:
+The key idea is to perform a binary search on the time it takes to reach a point (i, j) in the grid from the starting point (0, 0). Then, we can use depth-first search (DFS) to check if it's possible to reach the destination point (n-1, n-1) within that time limit.
+
+Approach:
+1. Initialize the lower bound as the minimum value in the grid and the upper bound as the maximum value in the grid.
+2. Perform a binary search on the time, searching for the minimum time required to reach the destination point (n-1, n-1) from the starting point (0, 0).
+3. In each binary search iteration:
+   - Calculate the mid-time (current guess) as the average of the lower and upper bounds.
+   - Create a visited array of size n x n to keep track of visited cells.
+   - Start DFS from the starting point (0, 0) and explore cells while considering two conditions:
+     - The cell is within bounds (0 <= i < n and 0 <= j < n).
+     - The cell value is less than or equal to the current mid-time.
+   - If DFS reaches the destination point (n-1, n-1), update the upper bound with the mid-time.
+   - Otherwise, update the lower bound.
+4. Repeat the binary search until the lower bound is less than the upper bound.
+5. Return the final lower bound as the minimum time required to reach the destination.
+
+Time Complexity:
+O(n^2 * log(maximum value in the grid)), where n is the size of the grid.
+
+Space Complexity:
+O(n^2) for the visited array.
+*/
+
+impl Solution {
+    pub fn swim_in_water(grid: Vec<Vec<i32>>) -> i32 {
+        let n = grid.len();
+        let mut low = grid[0][0];
+        let mut high = (n * n) as i32 - 1;
+        
+        while low < high {
+            let mid = low + (high - low) / 2;
+            let mut visited = vec![vec![false; n]; n];
+            
+            if Self::dfs(&grid, &mut visited, 0, 0, mid) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        low
+    }
+    
+    fn dfs(grid: &Vec<Vec<i32>>, visited: &mut Vec<Vec<bool>>, i: usize, j: usize, time: i32) -> bool {
+        if i == grid.len() - 1 && j == grid[0].len() - 1 {
+            return true;
+        }
+        
+        visited[i][j] = true;
+        let directions = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+        let n = grid.len();
+        
+        for (dx, dy) in &directions {
+            let ni = i as i32 + dx;
+            let nj = j as i32 + dy;
+            
+            if ni >= 0 && ni < n as i32 && nj >= 0 && nj < n as i32 && !visited[ni as usize][nj as usize] && grid[ni as usize][nj as usize] <= time {
+                if Self::dfs(grid, visited, ni as usize, nj as usize, time) {
+                    return true;
+                }
+            }
+        }
+        
+        false
+    }
+}
---
+++ 12_Advanced_Graphs/06_Cheapest_Flights_Within_K_Stops/0787-cheapest-flights-within-k-stops.rs
@@ -0,0 +1,64 @@
+/*
+Problem: LeetCode 787 - Cheapest Flights Within K Stops
+
+Key Idea:
+The key idea is to use a modified form of Dijkstra's algorithm to find the cheapest price from the source to the destination while considering the maximum number of stops.
+
+Approach:
+1. Create an adjacency list representation of the graph where each node is an airport, and each edge is a flight with its cost. You can use a vector of vectors for this purpose. The index of the outer vector represents the airport, and each inner vector contains pairs (to, price) representing flights from that airport.
+2. Initialize a vector stops to keep track of the minimum number of stops required to reach each airport from the source
+3. Initialize all elements to i32::MAX initially, except for the source airport, which should be set to 0.
+4. Initialize a priority queue (min-heap) min_heap to store tuples (total_price, airport, stops) in reverse order of total_price (i.e., lowest total_price comes first).
+5. Push a tuple (0, src, 0) onto the min_heap, where 0 is the total price, src is the source airport, and 0 is the number of stops.
+6. Explore Nodes in Priority Queue:
+  - While the min_heap is not empty:
+      - Pop the tuple (total_price, from, steps) from the min_heap in reverse order.
+          - If steps exceeds k or if from is equal to dst, return total_price as the answer.
+          - Otherwise, update stops[from] with steps if steps is smaller than the current value.
+          - For each flight (to, price) from from to other airports:
+              - Calculate the new_total_price as total_price + price.
+              - Push the tuple (new_total_price, to, steps + 1) onto the min_heap.
+7. If no valid path is found after exploring all nodes, return -1.
+
+Time Complexity:
+O(E + V * log(V)), where E is the number of flights (edges) and V is the number of airports (nodes). The use of a priority queue ensures that we explore nodes in order of increasing total_price.
+
+Space Complexity:
+O(V), for stops vector and the min_heap.
+*/
+
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn find_cheapest_price(n: i32, flights: Vec<Vec<i32>>, src: i32, dst: i32, k: i32) -> i32 {
+        let mut graph = vec![vec![]; n as usize];
+        for flight in &flights {
+            let (from, to, price) = (flight[0] as usize, flight[1] as usize, flight[2]);
+            graph[from].push((to, price));
+        }
+
+        let mut stops = vec![i32::MAX; n as usize];
+
+        let mut min_heap = BinaryHeap::new();
+        min_heap.push(Reverse((0, src as usize, 0)));
+
+        while !min_heap.is_empty() {
+            let Reverse((total_price, from, steps)) = min_heap.pop().unwrap();
+
+            if steps > stops[from] || steps > k + 1 {
+                continue;
+            }
+            if from as i32 == dst {
+                return total_price;
+            }
+
+            stops[from] = steps;
+            for &(to, price) in &graph[from] {
+                min_heap.push(Reverse((total_price + price, to, steps + 1)));
+            }
+        }
+        
+        -1
+    }
+}
