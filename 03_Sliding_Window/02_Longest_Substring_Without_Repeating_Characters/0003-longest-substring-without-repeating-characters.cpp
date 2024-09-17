60fffd80ae15617cef934838ad11275b7c0f8fe8
mdmzfzl <mmuzakkir244@gmail.com>
2024-09-17 18:46:49 +0530

    Removed commented code

--- 03_Sliding_Window/02_Longest_Substring_Without_Repeating_Characters/0003-longest-substring-without-repeating-characters.cpp
+++ 03_Sliding_Window/02_Longest_Substring_Without_Repeating_Characters/0003-longest-substring-without-repeating-characters.cpp
@@ -55,30 +55,6 @@ class Solution {
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int result= 0;
-        unordered_set<char> chegg;
-        int i = 0, j = 0;
-
-        while(i < s.size()) {
-            if(chegg.find(s[i]) == chegg.end()) {
-                chegg.insert(s[i]);
-                i++;
-            }
-            else{
-                chegg.erase(s[j]);
-                j++;
-            }
-            result = max(result, i-j );
-        }
-        return result;
-    }
-};
-*/
-
-/*
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
         int n = s.length();
         int ans = 0;
         vector<int> M(256, -1);
